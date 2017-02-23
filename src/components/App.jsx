0e470e043041644efb0dca212a78d0a5da8c5a80
CraigRodrigues <craig@craigrodrigues.com>
2017-02-22 19:12:27 -0800

    Make VideoList clickable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer videos={window.exampleVideoData}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={window.exampleVideoData}/>
-    </div>
-  </div>
-);
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer videos={window.exampleVideoData}/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={window.exampleVideoData}/>
+//     </div>
+//   </div>
+// );
 
-ReactDOM.render(<App />, document.getElementById("app"));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoList: window.exampleVideoData,
+      currentVideo: window.exampleVideoData[0]
+    };
+
+    this.onVideoListEntryClick = this.onVideoListEntryClick.bind(this);
+  }
+
+  onVideoListEntryClick(video) {
+    this.setState({currentVideo: video});
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer currentVideo={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videoList} handleOnVideoListEntryClick={this.onVideoListEntryClick}/>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,19 +1,16 @@
-var VideoPlayer = (props) => {
+var VideoPlayer = (props) => (
 
-  var currentVideo = props.videos[0];
-
-  return (
     <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${currentVideo.id.videoId}`} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.currentVideo.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>{currentVideo.snippet.title}</h3>
-      <div>{currentVideo.snippet.description}</div>
+      <h3>{props.currentVideo.snippet.title}</h3>
+      <div>{props.currentVideo.snippet.description}</div>
     </div>
   </div>
-  );
-};
+  
+);
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
