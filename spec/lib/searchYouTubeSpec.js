84f1e282ceb3b08ef83c27e62672d3138d887095
CraigRodrigues <craig@craigrodrigues.com>
2017-02-22 20:29:17 -0800

    Add Youtube API key and searchYouTube function

--- spec/lib/searchYouTubeSpec.js
+++ spec/lib/searchYouTubeSpec.js
@@ -56,7 +56,7 @@ describe('searchYouTube', function() {
   });
 
   it('should accept `key`, `query`, and `max` options and send them in GET request', function() {
-    searchYouTube({ key: 'API_KEY', query: 'cats', max: 10 }, () => {});
+    searchYouTube({ key: 'API_KEY', query: 'cats', max: 10, part: 'snippet', type: 'video' }, () => {});
 
     var params = getURLSearchParams(requests[0].url);
     expect(params.key).to.equal('API_KEY');
@@ -70,7 +70,9 @@ describe('searchYouTube', function() {
     var options = {
       key: window.YOUTUBE_API_KEY,
       query: 'react',
-      max: 5
+      max: 5,
+      part: 'snippet',
+      type: 'video'
     };
 
     // We want this test to make a real AJAX request
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,16 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: options,
+    success: function(data) {
+      console.log(data);
+      callback(data);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
